acb907161937b67ab8de83eade524135421b89b6
Rafael Gaid Jr <rgaidjr@gmail.com>
2025-07-06 17:00:10 +0800

    added BID logo

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,8 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+      alt="BID logo"
+      src="~assets/bid-logo.jpg"
     >
   </q-page>
 </template>
